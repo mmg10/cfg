d7559370ce1ccefdd4933411aac6f0df7ddc8fa7
mmg10 <65535131+mmg10@users.noreply.github.com>
2022-11-25 18:12:59 +0500

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -4,6 +4,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 sudo curl -fsSL  https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -o /usr/bin/oh-my-posh
 sudo chmod +x /usr/bin/oh-my-posh
-wget https://raw.githubusercontent.com/mmg10/cfg/main/zshrc -O ~/.zshrc
-wget https://raw.githubusercontent.com/mmg10/cfg/main/gmay_mine.omp-ec2.json -O ~/.oh-my-zsh/gmay_mine.omp.json
-sudo chsh -s $(which zsh)
+cp .zshrc ~/.zshrc
+cp gmay_mine.omp-ec2.json ~/.oh-my-zsh/gmay_mine.omp.json
