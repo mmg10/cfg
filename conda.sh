aacdc009d7817c367bee2eb1c78d80d7e443af2e
mmg10 <65535131+mmg10@users.noreply.github.com>
2022-11-25 17:49:45 +0500

    Update conda.sh

--- conda.sh
+++ conda.sh
@@ -2,3 +2,4 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 echo 'PATH=~/miniconda3/bin:$PATH' >> ~/.profile
 source ~/.profile
+conda create --name default python=3.8
