a9bfab7d8df790d0aa8d24eacb986aa03560d81b
mmg10 <65535131+mmg10@users.noreply.github.com>
2022-11-25 18:25:34 +0500

    Create 04_docker.sh

---
+++ 04_docker.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
