fc20732e3799ea7d27eafb8e7ff8310d7566a1ab
- <->
2025-07-27 19:53:56 +0500

    updated to uv

--- 02_python.sh
+++ 02_python.sh
@@ -2,9 +2,19 @@ wget -qO- https://astral.sh/uv/install.sh | sh
 if ! command -v nvidia-smi &> /dev/null
 then
     echo "Running CPU only machine"
-    sudo apt install python3-pip python3-venv -y
-    python -m venv ~/envs/default
-    echo 'ven default' >> ~/.zshrc
+    #sudo apt install python3-pip python3-venv -y
+    cd ~/ven/default
+    uv venv
+    uv init .
+    mv .venv/* .
+    rm -rf .venv
+    ln -s ~/ven/default .venv
+    python -m venv ~/ven/default
+    source bin/activate
+    uv add --no-cache-dir pip
+    #echo 'ven default' >> ~/.zshrc
+    echo 'source ~/ven/default/bin/activate' >> ~/.zshrc
+    cd - > /dev/null
     exit
 else
     echo "Running GPU machine"
